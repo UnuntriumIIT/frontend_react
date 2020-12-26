f617ac038585e408121b488aa7e502c16c72b1e3
Andrew <andreidmarket@mail.ru>
2020-12-27 00:18:30 +0500

    admin

--- js/admin-script.js
+++ js/admin-script.js
@@ -1,18 +1,7 @@
 $(document).ready(
     function() {
-        $('#item1').show();
-        $('#item2').hide();
         $('.dia-back').hide();
         $('dialog').hide();
-        $('.dot__right').click(function(){
-            slideRight(); 
-        });
-
-        $('.dot__left').click(function(){ 
-            slideLeft();
-        });
-        setInterval(slide, 5000);
-
         $('.top-part__log-in-button').click(function(){
             OffScroll();
             $('.dia-back').show();
@@ -82,35 +71,3 @@ function closeModal(){
     $('#pswd').val('');
     $(window).unbind('scroll');
 }
-
-function slideRight(){
-    $('#item1').fadeOut();
-    sleep(500);
-    $('#item2').fadeIn();
-    document.getElementsByClassName('dot__left')[0].style.background='#264160';
-    document.getElementsByClassName('dot__right')[0].style.background='#346491';
-}
-
-function slideLeft(){
-    $('#item2').fadeOut();
-    sleep(500);
-    $('#item1').fadeIn();
-    document.getElementsByClassName('dot__left')[0].style.background='#346491';
-    document.getElementsByClassName('dot__right')[0].style.background='#264160';
-}
-
-var next = false;
-function slide(){
-    if (next){
-        slideRight();
-    }
-    else{
-        slideLeft();
-    }
-    next = !next;
-}
-
-function sleep(ms) {
-    ms += new Date().getTime();
-    while (new Date() < ms){}
-} 
